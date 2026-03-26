fe7308f1fce107a153216c1f5504eaf57b2024b9
LisaraHansali <lisarahansali@gmail.com>
2026-03-26 20:01:37 +0530

    auto resize text area

--- addProduct.php
+++ addProduct.php
@@ -682,8 +682,14 @@ $conditions = Database::getMultipleRows("SELECT * FROM product_conditions WHERE 
                 toast.remove();
             });
         }
-        
 
+        // Auto-resize textarea
+        const textarea = document.getElementById('description');
+        textarea.addEventListener('input', function() {
+            this.style.height = 'auto';
+            this.style.height = this.scrollHeight + 'px';
+        });
+        
 
     </script>
 </body>
